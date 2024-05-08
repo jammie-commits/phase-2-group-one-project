e08a1f264f99edf38c294aed2b81226a7456f080
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-05-08 09:48:56 +0300

    fourth  commit

--- recipe-finder/src/components/MealItem.jsx
+++ recipe-finder/src/components/MealItem.jsx
@@ -10,7 +10,13 @@ function MealItem() {
     <>
     {
         (!data)?"Not Found" :data.map(item =>{
-            
+            return(
+                <div className="card" key={item.idMeal} onClick={()=>{navigate(`/${item.idMeal}`)}}>
+                  <img src={item.strMealThumb} alt=""/>
+                  <h3>{item.strMeal}</h3>
+                  </div>
+            )
+
         })
     }
     </>
