26379e4309a8c1ce5dd28a39831bf63b2581d0e9
jammie-commits <jay.mbugua.ph@gmail.com>
2024-05-08 16:03:42 +0300

    modified RecipeInfo

--- recipe-finder/src/components/RecipeInfo.jsx
+++ recipe-finder/src/components/RecipeInfo.jsx
@@ -12,5 +12,48 @@ const RecipeInfo=()=> {
         setItem(data.meals[0])
        })
     }
-}
+    
+    return (
+        <>
+          {
+            (!item)?"":(<>
+            <div className="content">
+                 <img src={item.strMealThumb} alt="" />
+                 <div className="inner-content">
+                      <h1>{item.strMeal}</h1>
+                      <h2>{item.strArea} Food</h2>
+                      <h3>Category{item.strCategory}</h3>
+                 </div>
+             </div>
+            <div className="recipe-details">
+                 <div className="ingredients">
+                  <h2>Ingredients</h2><br/>
+                    
+                      {Object.keys(item).map((key, index) => {
+    
+                   if (key.includes('strIngredient') && item[key]) {
+                    const ingredientNumber = key.slice(-1);
+                    const measureKey = `strMeasure${ingredientNumber}`;
+                    const measure = item[measureKey];
+                    return (
+                    <h4 key={index}>{item[key]}: {measure}</h4>
+                   );
+                  }
+                   return null;
+                  })}
+    
+                 </div>
+                 <div className="instructions">
+                      <h2>Instructions</h2><br/>
+                      <h4>{item.strInstructions}</h4>
+                 </div>
+            </div>
+            </>)
+          }
+    
+        </>
+      )
+    }
+
+
     export default RecipeInfo
