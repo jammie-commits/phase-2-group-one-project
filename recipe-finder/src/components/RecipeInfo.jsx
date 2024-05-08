d765fb75fbb300351f004ceaaa951844a920d71d
jammie-commits <jay.mbugua.ph@gmail.com>
2024-05-08 15:32:17 +0300

    updated RecipeInfo

--- recipe-finder/src/components/RecipeInfo.jsx
+++ recipe-finder/src/components/RecipeInfo.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function RecipeInfo() {
-  return (
-    <div>RecipeInfo</div>
-  )
+const RecipeInfo=()=> {
+    const [item,setItem]=useState();
+    const {MealId}=useParams();
+  
+    if(MealId!=="")
+    {
+       fetch(`https:/www.themealdb.com/api/json/v1/1/lookup.php?i=${MealId}`)
+       .then(res=>res.json())
+       .then(data=>{
+        setItem(data.meals[0])
+       })
+    }
 }
-
-export default RecipeInfo
+    export default RecipeInfo
