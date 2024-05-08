bc0696faaa740f1bff8a66b3985c310a3f85a001
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-05-08 09:41:48 +0300

    second commit

--- recipe-finder/src/components/MealItem.jsx
+++ recipe-finder/src/components/MealItem.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+const MealItem =((data) =>{
+    console.log(data)
+});
+let navigate =useNavigate();
 
 function MealItem() {
   return (
