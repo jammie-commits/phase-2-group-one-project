5e8eb4861957f32f8a624c192832bf38b25f4ea4
jammie-commits <jay.mbugua.ph@gmail.com>
2024-05-08 16:12:44 +0300

    updated file

--- recipe-finder/src/components/Meal.jsx
+++ recipe-finder/src/components/Meal.jsx
@@ -1,9 +1,12 @@
 import React, { useState , useEffect} from "react";
 import MealItem from "./MealItem";
 import RecipeIndex from "./RecipeIndex";
+
 const Meal=()=>{
     const[url,setUrl]=useState("https:/www.themealdb.com/api/json/v1/1/search.php?f=a");
     const[item,setItem]=useState();
     const[show,setShow]=useState(false);
     const [search,setSearch]=useState("")
 }
+
+export default Meal
