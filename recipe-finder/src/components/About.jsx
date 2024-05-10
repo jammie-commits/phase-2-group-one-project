8e9ed3aa027550c7f7f038fe70d8124e2a361378
Gracey789 <Gmwende487@gmail.com>
2024-05-11 00:33:05 +0300

    created about

---
+++ recipe-finder/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="about-page">
+            <h1>About GROUP ONE FOOD COURT</h1>
+            <p>
+                This is a recipe search application that allows you to explore a vast database of recipes and filter them by the first letter of the name or search for specific recipes.
+            </p>
+            <p>
+                Data is fetched from [TheMealDB](https://www.themealdb.com/).
+            </p>
+        </div>
+    );
+};
+
+export default About;
